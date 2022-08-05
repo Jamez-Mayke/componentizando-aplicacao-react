d5df7a93227838fc4b011b7ed7c5b209e0ec8d3d
Jamez Mayke <jamezmayke@gmail.com>
2022-08-05 18:48:45 -0300

    Removendo conteúdo dos componentes Sidebar e Content

--- src/App.tsx
+++ src/App.tsx
@@ -31,23 +31,9 @@ interface MovieProps {
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-  const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
   useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
-
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
@@ -59,9 +45,8 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-    
-      <SideBar />
-      <Content />
+      <SideBar handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
+      <Content selectedGenreId={selectedGenreId} selectedGenre={selectedGenre}  />
     </div>
   )
 }
